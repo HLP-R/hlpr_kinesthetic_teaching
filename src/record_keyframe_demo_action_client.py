91cb8e99d8a7f4a72b240fc645480579f832abed
Andrea Thomaz <athomaz@ece.utexas.edu>
2016-07-29 11:52:48 -0500

    traj demo record works now

--- src/record_keyframe_demo_action_client.py
+++ src/record_keyframe_demo_action_client.py
@@ -19,25 +19,26 @@ rospy.init_node('record_keyframe_demo_action_client')
 client = actionlib.SimpleActionClient('record_keyframe_demo', RecordKeyframeDemoAction)
 client.wait_for_server()
 
-time.sleep(1.0)
-
-raw_input("press any key to start a demo with 3 keyframes")
-
 goal = RecordKeyframeDemoGoal()
 goal.start_demo = 1.0
+
 goal.bag_file_name = 'demo_data_'+str(demo_num)
-print 'goal file name = '
-print goal.bag_file_name
 client.send_goal(goal, feedback_cb=feedback_call)
 
-raw_input("press any key to record keyframe number 1 (start)")
+
+print 'testing a KF demo with lots of frames, goal file name = '
+print goal.bag_file_name
+
 pub.publish(1)
 
-raw_input("press any key to record keyframe number 2 (mid)")
-pub.publish(2)
+frames = 0
+while frames < 100: 
+  pub.publish(2)
+  time.sleep(.1)
+  frames += 1
 
-raw_input("press any key to record keyframe number 2 (final)")
 pub.publish(3)
+demo_num += 1
 
 client.wait_for_result()
 print('[Result] State: %d'%(client.get_state()))
@@ -49,5 +50,66 @@ bag = rosbag.Bag(goal.bag_file_name)
 for topic, msg, t in bag.read_messages(topics=['joint_states', 'eef_pose']):
   print msg
 bag.close()
+
+
+################
+
+
+
+goal.bag_file_name = 'demo_data_'+str(demo_num)
+client.send_goal(goal, feedback_cb=feedback_call)
+
+raw_input("press any key to start a demo with 3 keyframes")
+
+print 'goal file name = '
+print goal.bag_file_name
+
+raw_input("press any key to record keyframe number 1 (start)")
+pub.publish(1)
+
+raw_input("press any key to record keyframe number 2 (mid)")
+pub.publish(2)
+
+raw_input("press any key to record keyframe number 2 (final)")
+pub.publish(3)
+
+demo_num += 1
+
+client.wait_for_result()
+print('[Result] State: %d'%(client.get_state()))
+print('[Result] Status: %s'%(client.get_goal_status_text()))
+print('[Result] Num Keyframes Recorded: %f'%(client.get_result().num_keyframes))
+
+print 'see what the bag file is like:'
+bag = rosbag.Bag(goal.bag_file_name)
+for topic, msg, t in bag.read_messages(topics=['joint_states', 'eef_pose']):
+  print msg
+bag.close()
+
+
+####################
+
+print 'testing a trajectory demo'
+goal.bag_file_name = 'demo_data_'+str(demo_num)
+client.send_goal(goal, feedback_cb=feedback_call)
+
+time.sleep(1.0)
+pub.publish(0)
+time.sleep(1.0)
+pub.publish(3)
+
+demo_num += 1
+
+client.wait_for_result()
+print('[Result] State: %d'%(client.get_state()))
+print('[Result] Status: %s'%(client.get_goal_status_text()))
+print('[Result] Num Keyframes Recorded: %f'%(client.get_result().num_keyframes))
+
+print 'see what the bag file is like:'
+bag = rosbag.Bag(goal.bag_file_name)
+for topic, msg, t in bag.read_messages(topics=['joint_states', 'eef_pose']):
+  print msg
+bag.close()
+
 
 
